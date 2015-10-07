39997fbcbc53408d7e51477b1760d14511e34cad
Steve Mathews <stevenmathews.nz@gmail.com>
2015-10-07 16:24:08 +1300

    add not found header to generated 200.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var getConfig = require('hjs-webpack')
 var layout = require('./src/pre-render/layout')
 var head = require('./src/pre-render/head')
+var notFound = require('./src/pre-render/not-found')
 
 module.exports = getConfig({
   // entry point for the app
@@ -26,7 +27,8 @@ module.exports = getConfig({
   // takes over when downloaded.
   html: function (data) {
     return {
-      'index.html': data.defaultTemplate({html: layout, head: head})
+      'index.html': data.defaultTemplate({html: layout, head: head}),
+      '200.html': data.defaultTemplate({html: notFound})
     }
   }
 })
