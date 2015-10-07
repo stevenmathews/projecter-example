9049cfa04841cf3427df250a32eba94fef1b692d
Steve Mathews <stevenmathews.nz@gmail.com>
2015-10-07 17:32:03 +1300

    add logo and favicon

--- src/pre-render/head.js
+++ src/pre-render/head.js
@@ -1,1 +1,4 @@
-module.exports = '<title>Project Starter</title>'
+module.exports = [
+  '<title>Project Starter</title>',
+  '<link rel="shortcut icon" href="http://s12.postimg.org/xvnmsnyuh/favicon.png" type="image/x-icon"/>'
+].join('')
