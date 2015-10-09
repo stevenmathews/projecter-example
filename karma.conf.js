6f70d01a1214e1d1e5f4446271f6a45538ebc25a
Steve Mathews <stevenmathews.nz@gmail.com>
2015-10-09 15:50:37 +1300

    add webpack with sourcemaps for preprocessing

--- karma.conf.js
+++ karma.conf.js
@@ -1,13 +1,15 @@
 // Karma configuration
 'use strict'
 
+const webpackConfigLoaders = require('./webpack.config.js').module.loaders
+
 module.exports = function (config) {
   config.set({
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
     // list of files / patterns to load in the browser
     files: [
-      'test/**/*.js'
+      'test/test-index.js'
     ],
     // list of files to exclude
     exclude: [
@@ -22,6 +24,21 @@ module.exports = function (config) {
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['mocha'],
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'test/test-index.js': ['webpack', 'sourcemap']
+    },
+    // options for 'karma-webpack'
+    webpack: {
+      module: {
+        loaders: webpackConfigLoaders // use the loaders from webpack.config.js
+      },
+      devtool: 'inline-source-map' // create source maps
+    },
+    webpackMiddleware: {
+      noInfo: true // stop webpack from logging the build process to console
+    },
     // web server port
     port: 9876,
     // enable / disable colors in the output (reporters and logs)
