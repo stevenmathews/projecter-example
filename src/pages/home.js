410976b7d48a5c80a625c1e48309309b2e44303a
Steve Mathews <stevenmathews.nz@gmail.com>
2015-10-09 18:10:22 +1300

    add package logos

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,30 +6,35 @@ export default React.createClass({
   render () {
     return (
       <div className='content'>
-        <a className='github-link' href='https://github.com/stevenmathews/projecter' target='_blank'>
-          <h2>Base for new projects to save time</h2>
-          <div className='github-logo'></div>
-        </a>
-        <ul>
-          <li>
+        <div className='summary'>
+          <a className='github-link' href='https://github.com/stevenmathews/projecter' target='_blank'>
+            <h2>Base for new projects to save time</h2>
+            <div className='github-logo'></div>
+          </a>
+        </div>
+        <div className='packages'>
+          <div className='webpack'>
             <a href='https://webpack.github.io/' target='_blank'>
+              <div className='webpack-logo'></div>
               <h4>Webpack</h4>
               <p>Configured with hotloading react and ES6 using Babel</p>
             </a>
-          </li>
-          <li>
+          </div>
+          <div className='karma'>
             <a href='https://karma-runner.github.io/' target='_blank'>
+              <div className='karma-logo'></div>
               <h4>Karma</h4>
               <p>Test runner that allows you to execute JavaScript code in multiple real browsers</p>
             </a>
-          </li>
-          <li>
+          </div>
+          <div className='travis'>
             <a href='https://travis-ci.org/' target='_blank'>
+              <div className='travis-logo'></div>
               <h4>Travis</h4>
               <p>Hosted continuous integration and deployment system</p>
             </a>
-          </li>
-        </ul>
+          </div>
+        </div>
       </div>
     )
   }
